9cab3c74cd4436503fc282ffe48366e40a1eb03a
pauline-wim <linette.wim@hotmail.com>
2022-01-29 13:03:05 +0100

    trying something different for the popup gallery window

--- script.js
+++ script.js
@@ -34,19 +34,32 @@ function close() {
 // POP-UP - SERIES PHOTO
 
 // const portraitBtn = document.getElementById('portraitBtn');
-const dredgeBtn = document.getElementById('dredgeBtn');
-const dredge = document.getElementById('dredge');
+// const dredgeBtn = document.getElementById('dredgeBtn');
+// const dredge = document.getElementById('dredge');
 
-dredgeBtn.addEventListener('click', openDredge);
-dredge.addEventListener('click', closeDredge);
-window.removeEventListener('click', closeDredge);
+// dredgeBtn.addEventListener('click', openDredge);
+// dredge.addEventListener('click', closeDredge);
+// window.removeEventListener('click', closeDredge);
 
-function openDredge() {
-  dredge.style.display = 'flex';
-  // dredge.setAttribute('style', 'display: flex');
-}
+// function openDredge() {
+//   dredge.style.display = 'flex';
+//   // dredge.setAttribute('style', 'display: flex');
+// }
 
-function closeDredge() {
-  // dredge.setAttribute('style', 'display: none');
-  dredge.style.display = 'none';
-}
+// function closeDredge() {
+//   // dredge.setAttribute('style', 'display: none');
+//   dredge.style.display = 'none';
+// }
+
+function DredgeGallery() {
+  console.log("onclick call function dredgeGallery")
+  const dredge = document.getElementById('dredge');
+
+  if (dredge.style.display === "flex") {
+    console.log("if display already flex, change display to none")
+    dredge.style.display === "none";
+  } else {
+    console.log("else change display to flex")
+    dredge.style.display === "flex";
+  }
+};
